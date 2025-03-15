ac6cfb2f1bcf0aaff1c5f988eb9effff060f651a
natanon <natanon.jari@bumail.net>
2025-03-15 15:17:59 +0700

    Add Script Magic

--- Assets/Scripts/VFXManager.cs
+++ Assets/Scripts/VFXManager.cs
@@ -5,8 +5,15 @@ public class VFXManager : MonoBehaviour
     [SerializeField] private GameObject doubleRingMarker;
     public GameObject DoubleRingMarker { get { return doubleRingMarker; } }
 
+    [SerializeField] private GameObject[] magicVFX;
+    public GameObject[] MagicVFX { get { return magicVFX; } }
+
     public static VFXManager instance;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        instance = this;
+    }
     void Start()
     {
         instance = this;
@@ -16,5 +23,25 @@ public class VFXManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void LoadMagicVFX(int id, Vector3 posA, float time)
+    {
+        //Load Magic
+        if (magicVFX[id] != null)
+            return;
+        GameObject objLoad = Instantiate(MagicVFX[id], posA, Quaternion.identity);
+        Destroy(objLoad, time);
+    }
+
+    public void ShootMagic(int id, Vector3 posA, Vector3 posB, float time)
+    {
+        //Shoot Magic
+        if (magicVFX[id] == null)
+            return;
+        
+        GameObject objShoot = Instantiate(magicVFX[id], posA, Quaternion.identity);
+        objShoot.transform.position = Vector3.LerpUnclamped(posA, posB, time);
+        Destroy(objShoot, time);
     }
 }
